83d78da48cb3daf0dfac55eb5e76b96e01598901
Yaphet Abraha <yaphet.abraha@gmail.com>
2019-02-11 08:03:40 -0600

    Added blueprint on health change event

--- Source/CoopHordeShooter/Public/Components/SHealthComponent.h
+++ Source/CoopHordeShooter/Public/Components/SHealthComponent.h
@@ -6,6 +6,8 @@
 #include "Components/ActorComponent.h"
 #include "SHealthComponent.generated.h"
 
+// OnHealthChanged event
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_SixParams(FOnHealthChangedSignature, USHealthComponent*, HealthComp, float, Health, float, HealthDelta, const class UDamageType*, DamageType, class AController*, InstigatedBy, AActor*, DamageCauser);
 
 UCLASS(ClassGroup = (COOP), meta = (BlueprintSpawnableComponent))
 class COOPHORDESHOOTER_API USHealthComponent : public UActorComponent
@@ -32,4 +34,7 @@ protected:
 	UFUNCTION()
 		void HandleTakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
 
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+		FOnHealthChangedSignature OnHealthChanged;
 };
