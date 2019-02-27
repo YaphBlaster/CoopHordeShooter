ec4144958580bdccd7999ca69750db0b3d416d09
Yaphet Abraha <yaphet.abraha@gmail.com>
2019-02-27 07:46:20 -0600

    powerup polish

--- Source/CoopHordeShooter/Public/SPickupActor.h
+++ Source/CoopHordeShooter/Public/SPickupActor.h
@@ -36,7 +36,7 @@ protected:
 
 	ASPowerupActor* PowerUpInstance;
 
-	UPROPERTY(EditDefaultsOnly, Category = "PickupActor")
+	UPROPERTY(EditInstanceOnly, Category = "PickupActor")
 		float CooldownDuration;
 
 	FTimerHandle TimerHandle_RespawnTimer;
