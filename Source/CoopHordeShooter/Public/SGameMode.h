5cb1b4b9020d4a242d81b8a79b672c1e43b82168
Yaphet Abraha <yaphet.abraha@gmail.com>
2019-02-28 08:14:00 -0600

    Add Wave Based Spawning

--- Source/CoopHordeShooter/Public/SGameMode.h
+++ Source/CoopHordeShooter/Public/SGameMode.h
@@ -7,11 +7,43 @@
 #include "SGameMode.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class COOPHORDESHOOTER_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+protected:
+	FTimerHandle TimerHandle_BotSpawner;
+
+	// Bots to spawn in current wave
+	int32 NumberOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+		float TimeBetweenWaves;
+
+protected:
+
+	// Mock for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+		void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	// Start Spawning Bots
+	void StartWave();
+
+	// Stop Spawning Bots
+	void EndWave();
+
+	// Set timer for next StartWave
+	void PrepareForNextWave();
+
+public:
+	ASGameMode();
+	virtual void StartPlay() override;
+
 };
